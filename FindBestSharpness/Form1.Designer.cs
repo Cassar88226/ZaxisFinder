33a44677101546d003feb4ba7cd462d7dbf474d7
figaro <figaro@users.noreply.github.com>
2019-11-30 01:48:18 +1000

    design UI 1. ImageViewer 2. ImageDialog

--- FindBestSharpness/Form1.Designer.cs
+++ FindBestSharpness/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace TestRTSP
+﻿namespace FindBestSharpness
 {
     partial class Form1
     {
@@ -28,73 +28,256 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
-            this.imageBox1 = new Emgu.CV.UI.ImageBox();
-            this.label_presentation_time = new System.Windows.Forms.Label();
+            this.dirBrowerBtn = new System.Windows.Forms.Button();
             this.label_cur_frame = new System.Windows.Forms.Label();
+            this.txtDirectory = new System.Windows.Forms.TextBox();
+            this.executeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.startY = new System.Windows.Forms.TextBox();
+            this.startX = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.sHeight = new System.Windows.Forms.TextBox();
+            this.sWidth = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.zAxis = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.flowLayoutPanelMain = new FindBestSharpness.ThumbnailFlowLayoutPanel();
+            this.trackBarSize = new System.Windows.Forms.TrackBar();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.panelMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSize)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // dirBrowerBtn
             // 
-            this.button1.Location = new System.Drawing.Point(674, 49);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_ClickAsync);
-            // 
-            // imageBox1
-            // 
-            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.imageBox1.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
-            this.imageBox1.Location = new System.Drawing.Point(100, 48);
-            this.imageBox1.Name = "imageBox1";
-            this.imageBox1.Size = new System.Drawing.Size(496, 318);
-            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.imageBox1.TabIndex = 2;
-            this.imageBox1.TabStop = false;
-            // 
-            // label_presentation_time
-            // 
-            this.label_presentation_time.Location = new System.Drawing.Point(624, 108);
-            this.label_presentation_time.Name = "label_presentation_time";
-            this.label_presentation_time.Size = new System.Drawing.Size(160, 23);
-            this.label_presentation_time.TabIndex = 3;
-            this.label_presentation_time.Text = "label1";
+            this.dirBrowerBtn.Location = new System.Drawing.Point(543, 9);
+            this.dirBrowerBtn.Name = "dirBrowerBtn";
+            this.dirBrowerBtn.Size = new System.Drawing.Size(75, 23);
+            this.dirBrowerBtn.TabIndex = 0;
+            this.dirBrowerBtn.Text = "Browser...";
+            this.dirBrowerBtn.UseVisualStyleBackColor = true;
+            this.dirBrowerBtn.Click += new System.EventHandler(this.dirBrowser);
             // 
             // label_cur_frame
             // 
-            this.label_cur_frame.Location = new System.Drawing.Point(624, 152);
+            this.label_cur_frame.Location = new System.Drawing.Point(10, 34);
             this.label_cur_frame.Name = "label_cur_frame";
-            this.label_cur_frame.Size = new System.Drawing.Size(160, 23);
+            this.label_cur_frame.Size = new System.Drawing.Size(49, 23);
             this.label_cur_frame.TabIndex = 3;
-            this.label_cur_frame.Text = "label1";
+            this.label_cur_frame.Text = "Start X :";
+            // 
+            // txtDirectory
+            // 
+            this.txtDirectory.Location = new System.Drawing.Point(12, 11);
+            this.txtDirectory.Name = "txtDirectory";
+            this.txtDirectory.Size = new System.Drawing.Size(525, 20);
+            this.txtDirectory.TabIndex = 4;
+            this.txtDirectory.Text = "Select the Directory";
+            // 
+            // executeButton
+            // 
+            this.executeButton.Enabled = false;
+            this.executeButton.Location = new System.Drawing.Point(639, 9);
+            this.executeButton.Name = "executeButton";
+            this.executeButton.Size = new System.Drawing.Size(75, 23);
+            this.executeButton.TabIndex = 0;
+            this.executeButton.Text = "Start";
+            this.executeButton.UseVisualStyleBackColor = true;
+            this.executeButton.Click += new System.EventHandler(this.Start_Finder);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(10, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Start Y :";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.startY);
+            this.groupBox1.Controls.Add(this.startX);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label_cur_frame);
+            this.groupBox1.Location = new System.Drawing.Point(639, 49);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(208, 105);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Start Point";
+            // 
+            // startY
+            // 
+            this.startY.Location = new System.Drawing.Point(65, 67);
+            this.startY.Name = "startY";
+            this.startY.Size = new System.Drawing.Size(100, 20);
+            this.startY.TabIndex = 4;
+            this.startY.Text = "0";
+            this.startY.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.checkValidInput);
+            // 
+            // startX
+            // 
+            this.startX.Location = new System.Drawing.Point(65, 33);
+            this.startX.Name = "startX";
+            this.startX.Size = new System.Drawing.Size(100, 20);
+            this.startX.TabIndex = 4;
+            this.startX.Text = "0";
+            this.startX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.checkValidInput);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.sHeight);
+            this.groupBox2.Controls.Add(this.sWidth);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(639, 169);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(208, 105);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Size";
+            // 
+            // sHeight
+            // 
+            this.sHeight.Location = new System.Drawing.Point(65, 67);
+            this.sHeight.Name = "sHeight";
+            this.sHeight.Size = new System.Drawing.Size(100, 20);
+            this.sHeight.TabIndex = 4;
+            this.sHeight.Text = "100";
+            this.sHeight.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.checkValidInput);
+            // 
+            // sWidth
+            // 
+            this.sWidth.Location = new System.Drawing.Point(65, 33);
+            this.sWidth.Name = "sWidth";
+            this.sWidth.Size = new System.Drawing.Size(100, 20);
+            this.sWidth.TabIndex = 4;
+            this.sWidth.Text = "100";
+            this.sWidth.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.checkValidInput);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(10, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Height :";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(10, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Width :";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Controls.Add(this.zAxis);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Location = new System.Drawing.Point(639, 298);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(208, 105);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Result";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(100, 13);
+            this.textBox1.Location = new System.Drawing.Point(65, 67);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(496, 20);
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 4;
-            this.textBox1.Text = "rtsp://192.168.1.50/videoMain";
+            this.textBox1.Text = "100";
+            // 
+            // zAxis
+            // 
+            this.zAxis.Location = new System.Drawing.Point(65, 33);
+            this.zAxis.Name = "zAxis";
+            this.zAxis.Size = new System.Drawing.Size(100, 20);
+            this.zAxis.TabIndex = 4;
+            this.zAxis.Text = "100";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(10, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Height :";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(10, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 23);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Z axis :";
+            // 
+            // panelMain
+            // 
+            this.panelMain.Controls.Add(this.flowLayoutPanelMain);
+            this.panelMain.Location = new System.Drawing.Point(12, 49);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(606, 396);
+            this.panelMain.TabIndex = 7;
+            // 
+            // flowLayoutPanelMain
+            // 
+            this.flowLayoutPanelMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanelMain.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanelMain.Name = "flowLayoutPanelMain";
+            this.flowLayoutPanelMain.Size = new System.Drawing.Size(606, 396);
+            this.flowLayoutPanelMain.TabIndex = 0;
+            // 
+            // trackBarSize
+            // 
+            this.trackBarSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBarSize.AutoSize = false;
+            this.trackBarSize.LargeChange = 1;
+            this.trackBarSize.Location = new System.Drawing.Point(723, 10);
+            this.trackBarSize.Maximum = 2;
+            this.trackBarSize.Name = "trackBarSize";
+            this.trackBarSize.Size = new System.Drawing.Size(124, 23);
+            this.trackBarSize.TabIndex = 6;
+            this.trackBarSize.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBarSize.Value = 1;
+            this.trackBarSize.ValueChanged += new System.EventHandler(this.trackBarSize_ValueChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label_cur_frame);
-            this.Controls.Add(this.label_presentation_time);
-            this.Controls.Add(this.imageBox1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(859, 461);
+            this.Controls.Add(this.trackBarSize);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtDirectory);
+            this.Controls.Add(this.executeButton);
+            this.Controls.Add(this.dirBrowerBtn);
             this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.Text = "Find_BestSharpness";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.panelMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSize)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,11 +285,27 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private Emgu.CV.UI.ImageBox imageBox1;
-        private System.Windows.Forms.Label label_presentation_time;
+        private System.Windows.Forms.Button dirBrowerBtn;
         private System.Windows.Forms.Label label_cur_frame;
+        private System.Windows.Forms.TextBox txtDirectory;
+        private System.Windows.Forms.Button executeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox startY;
+        private System.Windows.Forms.TextBox startX;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox sHeight;
+        private System.Windows.Forms.TextBox sWidth;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox zAxis;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.TrackBar trackBarSize;
+        private ThumbnailFlowLayoutPanel flowLayoutPanelMain;
     }
 }
 
--- FindBestSharpness/Form1.cs
+++ FindBestSharpness/Form1.cs
@@ -12,65 +12,245 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
-namespace TestRTSP
+using System.IO;
+
+namespace FindBestSharpness
 {
     public partial class Form1 : Form
     {
+
+        private string curDirectory;
+        private List<String> file_list;
+        private int file_index;
+        private VideoCapture m_vc_capture;
+        private Mat m_cur_frame;
+        private const int exifOrientationID = 0x112; //274
+
+
+        public event ThumbnailImageEventHandler OnImageSizeChanged;
+
+        private ThumbnailController m_Controller;
+
+        private ImageDialog m_ImageDialog;
+
+        private ImageViewer m_ActiveImageViewer;
+
+        private int ImageSize
+        {
+            get
+            {
+                return (64 * (this.trackBarSize.Value + 1));
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
-            m_timer_video.Interval = 30;//in default
-            m_timer_video.Tick += new EventHandler(ZDraw_Frame_from_timer);
-            pt1 = new Point(100, 100);
-            pt2 = new Point(300, 200);
+            curDirectory = null;
+            file_index = 0;
+            file_list = new List<string>();
+
+
+            m_ImageDialog = new ImageDialog();
+
+            m_AddImageDelegate = new DelegateAddImage(this.AddImage);
+
+            m_Controller = new ThumbnailController();
+            m_Controller.OnStart += new ThumbnailControllerEventHandler(m_Controller_OnStart);
+            m_Controller.OnAdd += new ThumbnailControllerEventHandler(m_Controller_OnAdd);
+            m_Controller.OnEnd += new ThumbnailControllerEventHandler(m_Controller_OnEnd);
+
         }
 
-        private void ZDraw_Frame_from_timer(object sender, EventArgs e)
+        private void m_Controller_OnStart(object sender, ThumbnailControllerEventArgs e)
         {
-            Mat frame = m_vc_capture.QueryFrame();
-            if(frame != null)
+
+        }
+
+        private void m_Controller_OnAdd(object sender, ThumbnailControllerEventArgs e)
+        {
+            this.AddImage(e.ImageFilename);
+            this.Invalidate();
+        }
+
+        private void m_Controller_OnEnd(object sender, ThumbnailControllerEventArgs e)
+        {
+            // thread safe
+            if (this.InvokeRequired)
             {
-                //double presen_time = m_vc_capture.GetCaptureProperty(CapProp.PosMsec);
-                //label_presentation_time.Text = ((int)presen_time).ToString();
-                //double frame_idx = m_vc_capture.GetCaptureProperty(CapProp.PosFrames);
-                //label_cur_frame.Text = ((int)frame_idx).ToString();
-                m_cur_frame = frame;
-                imageBox1.Image = Draw_Keypoints_To_Frame(frame);
+                this.Invoke(new ThumbnailControllerEventHandler(m_Controller_OnEnd), sender, e);
+            }
+            else
+            {
+                this.dirBrowerBtn.Enabled = true;
+            }
+        }
+
+        delegate void DelegateAddImage(string imageFilename);
+        private DelegateAddImage m_AddImageDelegate;
+        private void AddImage(string imageFilename)
+        {
+            // thread safe
+            if (this.InvokeRequired)
+            {
+                this.Invoke(m_AddImageDelegate, imageFilename);
+            }
+            else
+            {
+                int size = ImageSize;
+
+                ImageViewer imageViewer = new ImageViewer();
+                imageViewer.Dock = DockStyle.Bottom;
+                imageViewer.LoadImage(imageFilename, 256, 256);
+                imageViewer.Width = size;
+                imageViewer.Height = size;
+                imageViewer.IsThumbnail = true;
+                imageViewer.MouseClick += new MouseEventHandler(imageViewer_MouseClick);
+                imageViewer.SetFilename(imageFilename);
+
+
+                this.OnImageSizeChanged += new ThumbnailImageEventHandler(imageViewer.ImageSizeChanged);
+
+                this.flowLayoutPanelMain.Controls.Add(imageViewer);
+            }
+        }
+
+        private void imageViewer_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (m_ActiveImageViewer != null)
+            {
+                m_ActiveImageViewer.IsActive = false;
+            }
+
+            m_ActiveImageViewer = (ImageViewer)sender;
+            m_ActiveImageViewer.IsActive = true;
+
+            if (m_ImageDialog.IsDisposed) m_ImageDialog = new ImageDialog();
+            if (!m_ImageDialog.Visible) m_ImageDialog.Show();
+
+            m_ImageDialog.SetImage(m_ActiveImageViewer.ImageLocation);
+        }
+
+        private void trackBarSize_ValueChanged(object sender, EventArgs e)
+        {
+            this.OnImageSizeChanged(this, new ThumbnailImageEventArgs(ImageSize));
+        }
+
+
+        private void Start_Finder(object sender, EventArgs e)
+        {
+            string cur_filename = file_list.ElementAt(file_index++);
+            Image image = Image.FromFile(cur_filename);
+            ExifRotate(image);
+
+            Mat outMat;
+            if (file_index >= file_list.Count)
+            {
+                MessageBox.Show("Check finished", "State");
+                executeButton.Enabled = false;
+                return;
             }
 
         }
-
-        private VideoCapture m_vc_capture;
-        private System.Windows.Forms.Timer m_timer_video = new System.Windows.Forms.Timer();
-        private Point pt1 = new Point();
-        private Point pt2 = new Point();
-        private Mat m_cur_frame;
-        //private TimeSpan playedTime
-        private async void button1_ClickAsync(object sender, EventArgs e)
+        public static void ExifRotate(Image img)
         {
-            string file = @"e:\1.mp4";
-            //m_vc_capture = new VideoCapture(file);
-            //m_vc_capture = new VideoCapture("rtsp://172.16.1.31/video1.sdp");
-            m_vc_capture = new VideoCapture(textBox1.Text);
-            m_timer_video.Start();
-            while (true)
+            if (!img.PropertyIdList.Contains(exifOrientationID))
+                return;
+
+            var prop = img.GetPropertyItem(exifOrientationID);
+            int val = BitConverter.ToUInt16(prop.Value, 0);
+            var rot = RotateFlipType.RotateNoneFlipNone;
+
+            if (val == 3 || val == 4)
+                rot = RotateFlipType.Rotate180FlipNone;
+            else if (val == 5 || val == 6)
+                rot = RotateFlipType.Rotate90FlipNone;
+            else if (val == 7 || val == 8)
+                rot = RotateFlipType.Rotate270FlipNone;
+
+            if (val == 2 || val == 4 || val == 5 || val == 7)
+                rot |= RotateFlipType.RotateNoneFlipX;
+
+            if (rot != RotateFlipType.RotateNoneFlipNone)
+                img.RotateFlip(rot);
+        }
+        private void AddFolder()
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            dlg.Description = @"Choose folder path";
+            if (dlg.ShowDialog() == DialogResult.OK)
             {
-                await Task.Run(Do_Measure);
+                this.flowLayoutPanelMain.Controls.Clear();
+                m_Controller.AddFolder(dlg.SelectedPath);                
+                this.dirBrowerBtn.Enabled = false;
             }
         }
-        private Mat Draw_Keypoints_To_Frame(Mat aFrame)
-        {
-            CvInvoke.Line(aFrame, pt1, pt2, new Bgr(Color.Blue).MCvScalar, 3);
-            return aFrame;
-        }
-        private async Task Do_Measure()
-        {
-           Random rand = new Random();
-           await Task.Delay(rand.Next(1500, 5000));
-            pt1.Offset(rand.Next(-50, 50), rand.Next(-100, 100));
-            pt2.Offset(rand.Next(-50, 50), rand.Next(-100, 100));
-            label_presentation_time.Text = pt1.ToString() + ", " + pt2.ToString();
 
+        private void dirBrowser(object sender, EventArgs e)
+        {
+
+            this.AddFolder();
+
+            /*
+
+            file_index = 0;
+            FolderBrowserDialog dlgFolder = new FolderBrowserDialog();
+
+            dlgFolder.SelectedPath = curDirectory == null ? Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) : curDirectory;
+
+            if (dlgFolder.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+
+                curDirectory = txtDirectory.Text = dlgFolder.SelectedPath;
+                DirectoryInfo d = new DirectoryInfo(curDirectory);
+
+
+                file_list = new List<string>();
+                string[] Extensions = { "*.jpg", "*.png", "*.jfif" };
+                foreach (var ext in Extensions)
+                {
+                    FileInfo[] Files = d.GetFiles(ext);
+                    foreach (FileInfo file in Files)
+                    {
+                        file_list.Add(file.FullName);
+                    }
+                }
+            }
+            if (file_list.Count > 0)
+            {
+                executeButton.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("not found any image file. Please select other directory", "Warning");
+            }
+            */
+        }
+
+        private void checkValidInput(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
         }
     }
+    public class ThumbnailImageEventArgs : EventArgs
+    {
+        public ThumbnailImageEventArgs(int size)
+        {
+            this.Size = size;
+        }
+
+        public int Size;
+    }
+
+    public delegate void ThumbnailImageEventHandler(object sender, ThumbnailImageEventArgs e);
+
 }
