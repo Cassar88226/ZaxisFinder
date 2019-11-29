ef66db31ad33a45cd5390861bea5d678ff93ba18
figaro <figaro@users.noreply.github.com>
2019-11-29 17:00:32 +1000

    Create C# Project solution

---
+++ FindBestSharpness/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace TestRTSP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.label_presentation_time = new System.Windows.Forms.Label();
+            this.label_cur_frame = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(674, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_ClickAsync);
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox1.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            this.imageBox1.Location = new System.Drawing.Point(100, 48);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(496, 318);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // label_presentation_time
+            // 
+            this.label_presentation_time.Location = new System.Drawing.Point(624, 108);
+            this.label_presentation_time.Name = "label_presentation_time";
+            this.label_presentation_time.Size = new System.Drawing.Size(160, 23);
+            this.label_presentation_time.TabIndex = 3;
+            this.label_presentation_time.Text = "label1";
+            // 
+            // label_cur_frame
+            // 
+            this.label_cur_frame.Location = new System.Drawing.Point(624, 152);
+            this.label_cur_frame.Name = "label_cur_frame";
+            this.label_cur_frame.Size = new System.Drawing.Size(160, 23);
+            this.label_cur_frame.TabIndex = 3;
+            this.label_cur_frame.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(100, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(496, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "rtsp://192.168.1.50/videoMain";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label_cur_frame);
+            this.Controls.Add(this.label_presentation_time);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Label label_presentation_time;
+        private System.Windows.Forms.Label label_cur_frame;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ FindBestSharpness/Form1.cs
@@ -0,0 +1,76 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace TestRTSP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            m_timer_video.Interval = 30;//in default
+            m_timer_video.Tick += new EventHandler(ZDraw_Frame_from_timer);
+            pt1 = new Point(100, 100);
+            pt2 = new Point(300, 200);
+        }
+
+        private void ZDraw_Frame_from_timer(object sender, EventArgs e)
+        {
+            Mat frame = m_vc_capture.QueryFrame();
+            if(frame != null)
+            {
+                //double presen_time = m_vc_capture.GetCaptureProperty(CapProp.PosMsec);
+                //label_presentation_time.Text = ((int)presen_time).ToString();
+                //double frame_idx = m_vc_capture.GetCaptureProperty(CapProp.PosFrames);
+                //label_cur_frame.Text = ((int)frame_idx).ToString();
+                m_cur_frame = frame;
+                imageBox1.Image = Draw_Keypoints_To_Frame(frame);
+            }
+
+        }
+
+        private VideoCapture m_vc_capture;
+        private System.Windows.Forms.Timer m_timer_video = new System.Windows.Forms.Timer();
+        private Point pt1 = new Point();
+        private Point pt2 = new Point();
+        private Mat m_cur_frame;
+        //private TimeSpan playedTime
+        private async void button1_ClickAsync(object sender, EventArgs e)
+        {
+            string file = @"e:\1.mp4";
+            //m_vc_capture = new VideoCapture(file);
+            //m_vc_capture = new VideoCapture("rtsp://172.16.1.31/video1.sdp");
+            m_vc_capture = new VideoCapture(textBox1.Text);
+            m_timer_video.Start();
+            while (true)
+            {
+                await Task.Run(Do_Measure);
+            }
+        }
+        private Mat Draw_Keypoints_To_Frame(Mat aFrame)
+        {
+            CvInvoke.Line(aFrame, pt1, pt2, new Bgr(Color.Blue).MCvScalar, 3);
+            return aFrame;
+        }
+        private async Task Do_Measure()
+        {
+           Random rand = new Random();
+           await Task.Delay(rand.Next(1500, 5000));
+            pt1.Offset(rand.Next(-50, 50), rand.Next(-100, 100));
+            pt2.Offset(rand.Next(-50, 50), rand.Next(-100, 100));
+            label_presentation_time.Text = pt1.ToString() + ", " + pt2.ToString();
+
+        }
+    }
+}
